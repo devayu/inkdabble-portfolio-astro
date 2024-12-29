60e4c05818fcec69c6cf437c8e3f2a825f7c737c
Ayush Maurya <ayush.maurya@agilonhealth.com>
2024-12-29 18:25:07 +0530

    Added vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,14 @@ import { defineConfig, passthroughImageService } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({ applyBaseStyles: false })],
   // image: {
   //   service: passthroughImageService(),
   // },
+  integrations: [react(), tailwind({ applyBaseStyles: false })],
+
+  adapter: vercel(),
 });
